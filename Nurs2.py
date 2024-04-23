c144e3704fd1a6f7eb6b2c3f1d7b2b6581a0c953
nursultan20071 <zadyrasynovnursultan@gmail.com>
2024-04-23 13:13:05 +0300

    addition

---
+++ Nurs2.py
@@ -0,0 +1,5 @@
+def mutiple (x,y=10) :
+    print(x*y)
+    return x*y
+a=mutiple(4)
+print(a)
---
+++ Nurs3.py
@@ -0,0 +1,4 @@
+def find_max(list1):
+    return max(list1)
+a = find_max([1, 2, 3, 4, 5])
+print("Max:", a)
